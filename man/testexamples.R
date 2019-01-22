ff994e5dff8be5358dd1023bdf92d00aeae03f6b
Maikol Solís <maikol.solis@ucr.ac.cr>
2019-01-22 07:51:15 -0600

    Add VR and Delaunay examples

--- man/testexamples.R
+++ man/testexamples.R
@@ -2,7 +2,7 @@
 
 a <- -1
 b <- 1
-n <- 500
+n <- 1000
 X1 <- runif(n, a, b)
 X2 <- runif(n, a, b)
 Xnoise <- matrix(runif(n * 3, a, b), nrow = n)
@@ -14,127 +14,165 @@ Ydat <-data.frame(Y = 2 * X1 +  X2)
 
 
 message("Linear Case")
-ExLinear <- TopSA::TopSA(
+ExLinearVR <- TopSA::TopSA(
   Ydat = Ydat,
   Xdat = Xdat,
   dimension = 3,
-  threshold = 0.07,
+  threshold = 0.05,
   method = "VR"
 )
 
-pdf(file = "man/exLinear-reflection-1.pdf")
-plot(ExLinear,nvar = 1,with.reflection = TRUE)
+pdf(file = "man/exLinearVR-reflection-1.pdf")
+plot(ExLinearVR,nvar = 1,with.reflection = TRUE)
 dev.off()
-pdf(file = "man/exLinear-sym-diff-1.pdf")
-plot(ExLinear,nvar = 1,symmetric.diff = TRUE)
+pdf(file = "man/exLinearVR-sym-diff-1.pdf")
+plot(ExLinearVR,nvar = 1,symmetric.diff = TRUE)
 dev.off()
-pdf(file = "man/exLinear-reflection-2.pdf")
-plot(ExLinear,nvar = 2, with.reflection = TRUE)
+pdf(file = "man/exLinearVR-reflection-2.pdf")
+plot(ExLinearVR,nvar = 2, with.reflection = TRUE)
 dev.off()
-pdf(file = "man/exLinear-sym-diff-2.pdf")
-plot(ExLinear,nvar = 2, symmetric.diff = TRUE)
+pdf(file = "man/exLinearVR-sym-diff-2.pdf")
+plot(ExLinearVR,nvar = 2, symmetric.diff = TRUE)
 dev.off()
-pdf(file = "man/exLinear-reflection-3.pdf")
-plot(ExLinear,nvar = 3, with.reflection = TRUE)
+pdf(file = "man/exLinearVR-reflection-3.pdf")
+plot(ExLinearVR,nvar = 3, with.reflection = TRUE)
 dev.off()
-pdf(file = "man/exLinear-sym-diff-3.pdf")
-plot(ExLinear,nvar = 3, symmetric.diff = TRUE)
+pdf(file = "man/exLinearVR-sym-diff-3.pdf")
+plot(ExLinearVR,nvar = 3, symmetric.diff = TRUE)
 dev.off()
+
+
+
+message("Linear Case")
+ExLineardelanauy <- TopSA::TopSA(
+  Ydat = Ydat,
+  Xdat = Xdat,
+  dimension = 3,
+  threshold = 0.01,
+  method = "delanauy"
+)
+
+pdf(file = "man/exLineardelanauy-reflection-1.pdf")
+plot(ExLineardelanauy,nvar = 1,with.reflection = TRUE)
+dev.off()
+pdf(file = "man/exLineardelanauy-sym-diff-1.pdf")
+plot(ExLineardelanauy,nvar = 1,symmetric.diff = TRUE)
+dev.off()
+pdf(file = "man/exLineardelanauy-reflection-2.pdf")
+plot(ExLineardelanauy,nvar = 2, with.reflection = TRUE)
+dev.off()
+pdf(file = "man/exLineardelanauy-sym-diff-2.pdf")
+plot(ExLineardelanauy,nvar = 2, symmetric.diff = TRUE)
+dev.off()
+pdf(file = "man/exLineardelanauy-reflection-3.pdf")
+plot(ExLineardelanauy,nvar = 3, with.reflection = TRUE)
+dev.off()
+pdf(file = "man/exLineardelanauy-sym-diff-3.pdf")
+plot(ExLineardelanauy,nvar = 3, symmetric.diff = TRUE)
+dev.off()
+
+
+
+
+# a <- -1
+# b <- 1
+# n <- 1000
+# X1 <- runif(n, a, b)
+# X2 <- runif(n, a, b)
+# X3 <- runif(n, a, b)
+# X <- cbind(X1, X2, X3)
+# Y <- X1 +  X2^4
+# # plot(X[,1],Y)
+# # plot(X[,2],Y)
+#
+#
+# radius <- rep(2, times = ncol(X))
+#
+# message("Quartic Case")
+# ExQuartic <- sobolManifold(
+#   Y = Y,
+#   X = X,
+#   radius = radius,
+#   dimension = 3,
+#   alpha = 0.05
+# )
+#
+#
+# g1 <- ExQuartic$HOMOLOGY[[1]]
+# pdf(file = "man/exQuartic-1.pdf")
+# plot.sobolManifold(g1, n=10000)
+# dev.off()
+# g2 <- ExQuartic$HOMOLOGY[[2]]
+# pdf(file = "man/exQuartic-2.pdf")
+# plot.sobolManifold(g2, n=10000)
+# dev.off()
+# g3 <- ExQuartic$HOMOLOGY[[3]]
+# pdf(file = "man/exQuartic-3.pdf")
+# plot.sobolManifold(g3, n=10000)
+# dev.off()
+
+
+
 
 a <- -1
 b <- 1
 n <- 1000
-X1 <- runif(n, a, b)
-X2 <- runif(n, a, b)
-X3 <- runif(n, a, b)
-X <- cbind(X1, X2, X3)
-Y <- X1 +  X2^4
-# plot(X[,1],Y)
-# plot(X[,2],Y)
-
-
-radius <- rep(2, times = ncol(X))
-
-message("Quartic Case")
-ExQuartic <- sobolManifold(
-  Y = Y,
-  X = X,
-  radius = radius,
-  dimension = 3,
-  alpha = 0.05
-)
-
-
-g1 <- ExQuartic$HOMOLOGY[[1]]
-pdf(file = "man/exQuartic-1.pdf")
-plot.sobolManifold(g1, n=10000)
-dev.off()
-g2 <- ExQuartic$HOMOLOGY[[2]]
-pdf(file = "man/exQuartic-2.pdf")
-plot.sobolManifold(g2, n=10000)
-dev.off()
-g3 <- ExQuartic$HOMOLOGY[[3]]
-pdf(file = "man/exQuartic-3.pdf")
-plot.sobolManifold(g3, n=10000)
-dev.off()
-
-
-
-a <- -1
-b <- 1
-n <- 500
 theta <- runif(n, 0, 2*pi)
 r <- (sqrt(runif(n))) * (0.5) + 0.5
 X2 <- runif(n, a, b)
 X1 <- r*cos(theta)
-Y <- r*sin(theta)
-X <- cbind(X1, X2)
-# X <- scales::rescale(X, to = c(0, 1))
-# Y <- scales::rescale(Y, to = c(0, 1))
-# plot(X[,1],Y, asp = 1)
-# plot(X[,2],Y)
+Ydat <- data.frame(Y = r*sin(theta))
+Xdat <- data.frame(X1, X2)
 
-radius <- c(0.5,1)
 
 message("Circle 1 Case")
-ExCirc1hole <- TopSA::TopSA(
-  Y = Y,
-  X = X,
-  radius = radius,
+ExCirc1holeVR <- TopSA::TopSA(
+  Ydat = Ydat,
+  Xdat = Xdat,
   dimension = 3,
-  alpha = 0.05
+  threshold = 0.05,
+  knearest = 15,
+  method = "VR"
 )
 
-g1 <- ExCirc1hole$HOMOLOGY[[1]]
-pdf(file = "man/exCircle-1.pdf")
-plot.sobolManifold(g1, n=10000)
+pdf(file = "man/ExCirc1holeVR-reflection-1.pdf")
+plot(ExCirc1holeVR,nvar = 1,with.reflection = TRUE, asp = 1)
+dev.off()
+pdf(file = "man/ExCirc1holeVR-sym-diff-1.pdf")
+plot(ExCirc1holeVR,nvar = 1,symmetric.diff = TRUE)
+dev.off()
+pdf(file = "man/ExCirc1holeVR-reflection-2.pdf")
+plot(ExCirc1holeVR,nvar = 2, with.reflection = TRUE)
+dev.off()
+pdf(file = "man/ExCirc1holeVR-sym-diff-2.pdf")
+plot(ExCirc1holeVR,nvar = 2, symmetric.diff = TRUE)
 dev.off()
 
-g2 <- ExCirc1hole$HOMOLOGY[[2]]
-pdf(file = "man/exCircle-2.pdf")
-plot.sobolManifold(g2, n=10000)
+
+ExCirc1holedelanauy <- TopSA::TopSA(
+  Ydat = Ydat,
+  Xdat = Xdat,
+  dimension = 3,
+  threshold = 0.01,
+  method = "delanauy"
+)
+
+pdf(file = "man/ExCirc1holedelanauy-reflection-1.pdf")
+plot(ExCirc1holedelanauy,nvar = 1,with.reflection = TRUE, asp = 1)
+dev.off()
+pdf(file = "man/ExCirc1holedelanauy-sym-diff-1.pdf")
+plot(ExCirc1holedelanauy,nvar = 1,symmetric.diff = TRUE)
+dev.off()
+pdf(file = "man/ExCirc1holedelanauy-reflection-2.pdf")
+plot(ExCirc1holedelanauy,nvar = 2, with.reflection = TRUE)
+dev.off()
+pdf(file = "man/ExCirc1holedelanauy-sym-diff-2.pdf")
+plot(ExCirc1holedelanauy,nvar = 2, symmetric.diff = TRUE)
 dev.off()
 
-#
-# alphaVec <- seq(0.05, 0.5, by = 0.05)
-# ExCirc1holeAlpha <- NULL
-#
-# for (a in alphaVec) {
-#   circ <- sobolManifold(
-#     Y = Y,
-#     X = X,
-#     radius = radius,
-#     dimension = 3,
-#     alpha = a
-#   )
-#   ExCirc1holeAlpha <- cbind(a, circ$index[, 3])
-# }
 
 
-
-
-
-n <- 500
+n <- 1000
 theta <- runif(n, 0, 2*pi)
 r1 <- sqrt(runif(n)) + 1
 r2 <- sqrt(runif(n)) * 0.5 + 0.5
@@ -146,73 +184,135 @@ X1c2 <- r2*cos(theta)+1.5
 Yc2 <- r2*sin(theta)+1.5
 
 X1 <- c(X1c1, X1c2)
-Y <- c(Yc1, Yc2)
+Ydat <- data.frame(Y = c(Yc1, Yc2))
 
-X <- cbind(X1, X2)
-# X <- scales::rescale(X, to = c(0, 1))
-# Y <- scales::rescale(Y, to = c(0, 1))
-# plot(X[,1],Y, asp=1)
-# plot(X[,2],Y)
-
-radius <- c(0.5,1)
+Xdat <- data.frame(X1, X2)
 
 message("Circle 2 Case")
-ExCirc2holes <- TopSA::TopSA(
-  Y = Y,
-  X = X,
-  radius = radius,
+ExCirc2holesVR <- TopSA::TopSA(
+  Ydat = Ydat,
+  Xdat = Xdat,
   dimension = 3,
-  alpha = 0.05
+  threshold = 0.01,
+  knearest = 25,
+  method = "VR"
 )
 
-g1 <- ExCirc2holes$HOMOLOGY[[1]]
-pdf(file = "man/exCircle2holes-1.pdf")
-plot.sobolManifold(g1, n=10000)
+pdf(file = "man/ExCirc2holesVR-reflection-1.pdf")
+plot(ExCirc2holesVR,nvar = 1,with.reflection = TRUE, asp = 1)
+dev.off()
+pdf(file = "man/ExCirc2holesVR-sym-diff-1.pdf")
+plot(ExCirc2holesVR,nvar = 1,symmetric.diff = TRUE)
+dev.off()
+pdf(file = "man/ExCirc2holesVR-reflection-2.pdf")
+plot(ExCirc2holesVR,nvar = 2, with.reflection = TRUE)
+dev.off()
+pdf(file = "man/ExCirc2holesVR-sym-diff-2.pdf")
+plot(ExCirc2holesVR,nvar = 2, symmetric.diff = TRUE)
 dev.off()
 
-g2 <- ExCirc2holes$HOMOLOGY[[2]]
-pdf(file = "man/exCircle2holes-2.pdf")
-plot.sobolManifold(g2, n=10000)
+
+
+ExCirc2holesdelanauy <- TopSA::TopSA(
+  Ydat = Ydat,
+  Xdat = Xdat,
+  dimension = 3,
+  threshold = 0.01,
+  method = "delanauy"
+)
+
+pdf(file = "man/ExCirc2holesdelanauy-reflection-1.pdf")
+plot(ExCirc2holesdelanauy,nvar = 1,with.reflection = TRUE, asp = 1)
 dev.off()
+pdf(file = "man/ExCirc2holesdelanauy-sym-diff-1.pdf")
+plot(ExCirc2holesdelanauy,nvar = 1,symmetric.diff = TRUE)
+dev.off()
+pdf(file = "man/ExCirc2holesdelanauy-reflection-2.pdf")
+plot(ExCirc2holesdelanauy,nvar = 2, with.reflection = TRUE)
+dev.off()
+pdf(file = "man/ExCirc2holesdelanauy-sym-diff-2.pdf")
+plot(ExCirc2holesdelanauy,nvar = 2, symmetric.diff = TRUE)
+dev.off()
+
 
 
 a <- -pi
 b <- pi
-n <- 500
+n <- 1000
 X1 <- runif(n, a, b)
 X2 <- runif(n, a, b)
 X3 <- runif(n, a, b)
-X4 <- rnorm(n)
-X <- cbind(X1, X2, X3, X4)
-Y <- sensitivity::ishigami.fun(X)
-# X <- scales::rescale(X, to = c(0,1))
-# Y <- scales::rescale(Y, to = c(0,1))
-# plot(scales  s::rescale(X[,1], to=c(0,1)),rescale(Y, to=c(0,1)), asp = 1)
-# plot(scales::rescale(X[,2], to=c(0,1)),rescale(Y, to=c(0,1)), asp = 1)
-# plot(scales::rescale(X[,3], to=c(0,1)),rescale(Y, to=c(0,1)), asp = 1)
-# plot(X[,1],Y, asp = 0, xlim = c(-3.2, 3.2))
-# plot(X[,2],Y, asp = 0, xlim = c(-3.2, 3.2))
-# plot(X[,3],Y, asp = 0, xlim = c(-3.2, 3.2))
+X4 <- runif(n)
+Xdat <- data.frame(X1, X2, X3, X4)
+Ydat <- data.frame(Y = sensitivity::ishigami.fun(Xdat))
+
 
 message("Ishigami Case")
-ExIshigami <- TopSA::TopSA(
-  Y = Y,
-  X = X,
-  radius = NULL,
+ExIshigamiVR <- TopSA::TopSA(
+  Ydat =  Ydat,
+  Xdat = Xdat,
   dimension = 3,
-  alpha = 0.07
+  threshold = 0.03,
+  method = "VR"
 )
-g1 <- ExIshigami$HOMOLOGY[[1]]
-pdf(file = "man/exIshigami-1.pdf")
-plot.sobolManifold(g1, n=10000)
+
+pdf(file = "man/ExIshigamiVR-reflection-1.pdf")
+plot(ExIshigamiVR, nvar = 1, with.reflection = TRUE)
 dev.off()
-g2 <- ExIshigami$HOMOLOGY[[2]]
-pdf(file = "man/exIshigami-2.pdf")
-plot.sobolManifold(g2, n=10000)
+pdf(file = "man/ExIshigamiVR-sym-diff-1.pdf")
+plot(ExIshigamiVR, nvar = 1, symmetric.diff = TRUE)
 dev.off()
-g3 <- ExIshigami$HOMOLOGY[[3]]
-pdf(file = "man/exIshigami-3.pdf")
-plot.sobolManifold(g3, n=10000)
+pdf(file = "man/ExIshigamiVR-reflection-2.pdf")
+plot(ExIshigamiVR, nvar = 2, with.reflection = TRUE)
+dev.off()
+pdf(file = "man/ExIshigamiVR-sym-diff-2.pdf")
+plot(ExIshigamiVR, nvar = 2, symmetric.diff = TRUE)
+dev.off()
+pdf(file = "man/ExIshigamiVR-reflection-3.pdf")
+plot(ExIshigamiVR, nvar = 3, with.reflection = TRUE)
+dev.off()
+pdf(file = "man/ExIshigamiVR-sym-diff-3.pdf")
+plot(ExIshigamiVR, nvar = 3, symmetric.diff = TRUE)
+dev.off()
+pdf(file = "man/ExIshigamiVR-reflection-4.pdf")
+plot(ExIshigamiVR, nvar = 4, with.reflection = TRUE)
+dev.off()
+pdf(file = "man/ExIshigamiVR-sym-diff-4.pdf")
+plot(ExIshigamiVR, nvar = 4, symmetric.diff = TRUE)
+dev.off()
+
+
+ExIshigamidelanauy <- TopSA::TopSA(
+  Ydat =  Ydat,
+  Xdat = Xdat,
+  dimension = 3,
+  threshold = 0.02,
+  method = "delanauy"
+)
+
+pdf(file = "man/ExIshigamidelanauy-reflection-1.pdf")
+plot(ExIshigamidelanauy, nvar = 1, with.reflection = TRUE)
+dev.off()
+pdf(file = "man/ExIshigamidelanauy-sym-diff-1.pdf")
+plot(ExIshigamidelanauy, nvar = 1, symmetric.diff = TRUE)
+dev.off()
+pdf(file = "man/ExIshigamidelanauy-reflection-2.pdf")
+plot(ExIshigamidelanauy, nvar = 2, with.reflection = TRUE)
+dev.off()
+pdf(file = "man/ExIshigamidelanauy-sym-diff-2.pdf")
+plot(ExIshigamidelanauy, nvar = 2, symmetric.diff = TRUE)
+dev.off()
+pdf(file = "man/ExIshigamidelanauy-reflection-3.pdf")
+plot(ExIshigamidelanauy, nvar = 3, with.reflection = TRUE)
+dev.off()
+pdf(file = "man/ExIshigamidelanauy-sym-diff-3.pdf")
+plot(ExIshigamidelanauy, nvar = 3, symmetric.diff = TRUE)
+dev.off()
+pdf(file = "man/ExIshigamidelanauy-reflection-4.pdf")
+plot(ExIshigamidelanauy, nvar = 4, with.reflection = TRUE)
+dev.off()
+pdf(file = "man/ExIshigamidelanauy-sym-diff-4.pdf")
+plot(ExIshigamidelanauy, nvar = 4, symmetric.diff = TRUE)
 dev.off()
 
 
@@ -223,74 +323,4 @@ save(file = "man/examples.Rdata",
      ExLinear,
      ExQuartic)
 
-
-# library(VineCopula)
-# u <- pobs(as.matrix(cbind(X[,1],Y)))[,1]
-# v <- pobs(as.matrix(cbind(X[,2],Y)))[,2]
-# selectedCopula <- BiCopSelect(u,v,familyset=NA)
-# selectedCopula
-
-
-
-# X2[50] <- -1
-# X <- cbind(X1,X2)
-# Y <- X1 +  X2 ^ 2
-# Y[50] <- 0.5
-# plot(X1,Y)
-# plot(X2,Y)
-# plot(X3,Y)
-#
-# radius <- 0.2
-# dimension <- 3
-#
-# aa <- sobolManifold(
-#   Y = Y,
-#   X = X,
-#   radius = radius,
-#   dimension = 3
-# )
-#
-#
-#
-# g <- aa$HOMOLOGY[[1]]$graph
-# plot.igraph(
-#   mm,
-#   rescale = FALSE,
-#   axes = TRUE,
-#   asp = 0,
-#   ylim = range(V(g)$x),
-#   xlim = range(V(g)$y),
-#   vertex.size = 1,
-#   vertex.label.cex = 1,
-#   edge.arrow.size = 1,
-#   edge.width = 0.5
-# )
-#
-# g <- aa$HOMOLOGY[[2]]$graph
-# plot.igraph(
-#   g,
-#   rescale = FALSE,
-#   axes = TRUE,
-#   asp = 0,
-#   ylim = range(V(g)$x),
-#   xlim = range(V(g)$y),
-#   vertex.size = 1,
-#   vertex.label.cex = 1,
-#   edge.arrow.size = 1,
-#   edge.width = 0.5
-# )
-#
-# g <- aa$HOMOLOGY[[3]]$graph
-# plot.igraph(
-#   g,
-#   rescale = FALSE,
-#   axes = TRUE,
-#   asp = 0,
-#   ylim = range(V(g)$x),
-#   xlim = range(V(g)$y),
-#   vertex.size = 1,
-#   vertex.label.cex = 1,
-#   edge.arrow.size = 1,
-#   edge.width = 0.5
-# )
 
