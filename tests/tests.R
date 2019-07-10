3135963d2cbe89e1bc98bac26104331e6313beca
Maikol Solís <maikol.solis@ucr.ac.cr>
2019-07-10 10:36:42 +0200

    New test function

--- tests/tests.R
+++ tests/tests.R
@@ -1,26 +1,23 @@
 ishigami.fun <- function(X) {
-A <- 7
-B <- 0.1
-sin(X[, 1]) + A * sin(X[, 2])^2 + B * X[, 3]^4 * sin(X[, 1])
+  A <- 7
+  B <- 0.1
+  sin(X[, 1]) + A * sin(X[, 2]) ^ 2 + B * X[, 3] ^ 4 * sin(X[, 1])
 }
 
-a <- -pi
-b <- pi
-n <- 100
-X1 <- runif(n, a, b)
-X2 <- runif(n, a, b)
-X3 <- runif(n, a, b)
-Xdat <- data.frame(X1, X2, X3)
-Ydat <- data.frame(Y = ishigami.fun(Xdat))
+X <- matrix(runif(3 * 10, -pi, pi), ncol = 3)
+Y <- ishigami.fun(X)
 
+Ydat <- Y
+Xdat <- X
 
-message("Ishigami Case")
-ExIshigamiVR <- TopSA::TopSA(
-  Ydat =  Ydat,
-  Xdat = Xdat,
-  dimension = 3,
-  threshold = 0.01,
-  knearest = 25,
-  method = "VR",
-  mc.cores = 1
-)
+estimation <-
+  topsa::topsa(
+    Ydat = Y,
+    Xdat = X,
+    method = "VR",
+    threshold.radius = 0.1,
+    knearest = 25
+  )
+
+topsa::plot(estimation)
+topsa::print(estimation)
