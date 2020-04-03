fa1d507137fc9ace32ef56be6ad9d9db1dff7855
Maikol Solís <maikol.solis@ucr.ac.cr>
2020-04-03 14:43:08 -0600

    [WIP] Add testthat

--- tests/tests.R
+++ tests/tests.R
@@ -1,23 +1,27 @@
-ishigami.fun <- function(X) {
-  A <- 7
-  B <- 0.1
-  sin(X[, 1]) + A * sin(X[, 2]) ^ 2 + B * X[, 3] ^ 4 * sin(X[, 1])
-}
-
-X <- matrix(runif(3 * 20, -pi, pi), ncol = 3)
-Y <- ishigami.fun(X)
-
-Ydat <- Y
-Xdat <- X
-
-estimation <-
-  topsa::topsa(
-    Ydat = Y,
-    Xdat = X,
-    method = "VR",
-    threshold.radius = 0.2,
-    knearest = 25
-  )
-
-topsa::plot(estimation)
-topsa::print(estimation)
+# library(topsa)
+# library(ggplot2)
+#
+# # Linear example
+#
+# linear.fun <- function(X) {
+#   - 2 * X[, 1] +  X[, 2]
+# }
+#
+# X <- matrix(runif(3 * 1000, -1, 1), ncol = 3)
+# Y <- linear.fun(X)
+#
+# estimation <-
+#   topsa::topsa(
+#     Ydat = Y,
+#     Xdat = X,
+#     method = "VR",
+#     threshold.radius = 0.05,
+#     knearest = 10
+#   )
+#
+# p <- topsa::plot(estimation)
+# p[[1]]
+# p[[2]]
+# p[[3]]
+# topsa::print(estimation)
+# un
